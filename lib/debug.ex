f993fd6bfa7821e96325d14ebbbf4d3a40562cd2
Mayel de Borniol <git@mayel.space>
2022-06-06 13:22:48 +1200

    debug

--- lib/debug.ex
+++ lib/debug.ex
@@ -1,21 +1,23 @@
 defmodule Bonfire.Epics.Debug do
 
-  import Where
+  require Logger
   alias Bonfire.Epics.{Act, Epic}
+
+  @level :info # TODO: configurable
 
   def do_maybe_debug(nil, %Act{} = act, thing, label) do
     if act.options[:debug],
-      do: warn(thing, label)
+      do: Logger.log(@level, "#{label} #{inspect thing}")
   end
 
   def do_maybe_debug(nil, %Epic{} = epic, thing, label) do
     if epic.assigns[:options][:debug],
-      do: warn(thing, label)
+      do: Logger.log(@level, "#{label} #{inspect thing}")
   end
 
   def do_maybe_debug(%Epic{} = epic, %Act{} = act, thing, label) do
     if epic.assigns[:options][:debug] || act.options[:debug],
-      do: warn(thing, label)
+      do: Logger.log(@level, "#{label} #{inspect thing}")
   end
 
 end
