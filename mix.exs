3c3a5effd39f3a011c72a98519a9f81a19ab478e
Mayel de Borniol <git@mayel.space>
2022-11-29 14:29:36 +1300

    Function as multi repo umbrella

--- mix.exs
+++ mix.exs
@@ -1,7 +1,20 @@
+Code.eval_file("mess.exs", (if File.exists?("../../lib/mix/mess.exs"), do: "../../lib/mix/"))
+
 defmodule BonfireEpics.MixProject do
   use Mix.Project
 
   def project do
+    if File.exists?("../../.is_umbrella.exs") do
+      [
+        build_path: "../../_build",
+        config_path: "../../config/config.exs",
+        deps_path: "../../deps",
+        lockfile: "../../mix.lock"
+      ]
+    else
+      []
+    end
+    ++
     [
       app: :bonfire_epics,
       version: "0.1.0",
@@ -16,7 +29,7 @@ defmodule BonfireEpics.MixProject do
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
-    [
+    Mess.deps [
       {:untangle, "~> 0.1"},
       {:arrows,
        git: "https://github.com/bonfire-networks/arrows", branch: "main"},
