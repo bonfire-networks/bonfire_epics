2f9d527f912681699d6e90fe8089e58588a2de9f
Mayel de Borniol <git@mayel.space>
2024-02-29 14:35:00 +0000

    e

--- lib/error.ex
+++ lib/error.ex
@@ -1,22 +1,22 @@
 defmodule Bonfire.Epics.Error do
   import Untangle
 
+  alias Bonfire.Common.Errors
+
   defexception [:error, :source, :act, :epic, :stacktrace]
 
   def message(self) do
-    error = Bonfire.Common.Errors.error_msg(self.error)
-
     # if Bonfire.Common.Config.env() !=:prod do
     # throw is the one that will render anything, default to it.
     kind = if self.source in [:error, :exit], do: self.source, else: :throw
-    banner = Exception.format_banner(kind, error, self.stacktrace)
+    banner = Errors.format_banner(kind, self.error, self.stacktrace)
 
     error(self.act, banner)
     debug(self.stacktrace, "Act stacktrace")
     debug(self.epic.assigns, "Act assigns")
 
     # else
-    error
+    Errors.error_msg(self.error)
     # end
   end
 end
